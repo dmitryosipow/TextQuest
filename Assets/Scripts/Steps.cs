7eebc8d35179151070ff8ca3fa3cdca249cac4bc
dmitryosipow <dimaosipow@yahoo.com>
2020-12-29 17:58:11 +0300

    Initial commit

---
+++ Assets/Scripts/Steps.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Steps : MonoBehaviour
+{
+    [TextArea(10,50)]
+    public string content;
+    public string locate;
+
+    public Sprite background; 
+
+    public Steps nextSteps1;
+    public Steps nextSteps2;
+
+}
---
+++ Assets/Scripts/TextQuest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextQuest : MonoBehaviour
+{
+    public Text content;
+    public Text titleText;
+    public Text location;
+
+    public Image picture;
+
+    public Steps currentStep;
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleText.text = "Post-apocaleptic Minsk";
+        UpdateStep(currentStep);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            UpdateStep(currentStep.nextSteps1);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            UpdateStep(currentStep.nextSteps2);
+        }
+    }
+
+    void UpdateStep(Steps step)
+    {
+        currentStep = step;
+        content.text = step.content;
+        picture.sprite = step.background;
+        location.text = step.locate;
+    }
+}
