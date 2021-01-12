44dd1d91bb16347fad2cfe98a9bc309696721419
Changes from fungi <31540854+AenHenIchaer@users.noreply.github.com>
2021-01-12 21:34:34 +0300

    SO added

---
+++ Assets/Scripts/StepsSO.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class StepsSO : ScriptableObject
+{
+    [TextArea(10, 50)]
+    public string content;
+    public string locate;
+
+    public Sprite background;
+
+    public int deltaHealth = 0;
+    public bool startStep = false;
+
+    public StepsSO nextSteps1;
+    public StepsSO nextSteps2;
+}
---
+++ Assets/Scripts/TextQuestSO.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextQuestSO : MonoBehaviour
+{
+    public Text content;
+    public Text titleText;
+    public Text location;
+
+    public Image picture;
+
+    public StepsSO currentStep;
+    public PlayerControl player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleText.text = "Post-apocaleptic Minsk";
+        UpdateStep(currentStep);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            UpdateStep(currentStep.nextSteps1);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            UpdateStep(currentStep.nextSteps2);
+        }
+    }
+
+    private void Reset()
+    {
+        player.Reset();
+    }
+
+    void UpdateStep(StepsSO step)
+    {
+        currentStep = step;
+        content.text = step.content;
+        picture.sprite = step.background;
+        location.text = step.locate;
+        player.UpdateHealth(step.deltaHealth);
+        if (step.startStep)
+        {
+            Reset();
+        }
+    }
+}
